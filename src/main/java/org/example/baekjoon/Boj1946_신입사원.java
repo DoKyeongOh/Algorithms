0effacc0d4a5761d68ba47993aac8931d88a8ab4
dokyeong <dkproh@gmail.com>
2022-10-01 17:10:05 +0900

    실제 입력받을 수 있도록 솔루션 메서드 + 파일로부터 입력받을 수 있는 main 메서드 작성

---
+++ src/main/java/org/example/baekjoon/Boj1946_신입사원.java
@@ -0,0 +1,39 @@
+package org.example.baekjoon;
+
+import java.io.*;
+
+public class Boj1946_신입사원 {
+    public static void main(String[] arg) throws Exception {
+        File file = new File("src/main/resources/input");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        solution(br);
+    }
+
+    public static void solution(BufferedReader br) throws IOException {
+        int testCount = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCount; i++) {
+
+            int testCase = Integer.parseInt(br.readLine());
+            int[] arr1 = new int[testCase];
+
+            for (int n = 0; n < testCase; n++) {
+                String[] scores = br.readLine().split(" ");
+                arr1[Integer.parseInt(scores[0])-1] = Integer.parseInt(scores[1]);
+            }
+
+            int count = 0;
+            int std = arr1[0];
+            for (int n = 0; n < testCase; n++) {
+                if (arr1[n] <= std) {
+                    std = arr1[n];
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
+
+    }
+}
