376c99fe88aff7be1e7a1b345f6747a464af9363
dokyeong <dkproh@gmail.com>
2022-10-05 17:11:25 +0900

    minor

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -15,6 +20,22 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        boolean successed = true;
+        try {
+            // test content
+            List<String> list = new ArrayList<>();
+            list.add("3");
+            list.add("25");
+            list.add("1");
+            list.add("6");
+            list.add("12");
+            Collections.sort(list);
+            list.forEach(System.out::println);
+        }catch (Exception e) {
+            e.printStackTrace();
+            successed = false;
+        }
+        assertEquals(true, successed);
+
     }
 }
