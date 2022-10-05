869b6b9509a132b918e31d88591cc9bbdfd26926
dokyeong <dkproh@gmail.com>
2022-10-05 17:12:45 +0900

    bfs를 사용한 최단경로 찾기 해결 - 백준에 제출하니 잘 안되는데 다시 봐볼 필요가 있음.

--- src/main/java/org/example/baekjoon/Boj1260.java
+++ src/main/java/org/example/baekjoon/Boj1260.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class Boj1260 {
 
-    static Map<String, List<String>> graph;
+    static Map<Integer, List<Integer>> graph;
 
     public static void main(String[] arg) throws Exception {
         File file = new File("src/main/resources/input");
@@ -21,69 +21,69 @@ public class Boj1260 {
         String[] items = br.readLine().split(" ");
         int pointCount = Integer.parseInt(items[0]);
         int lineCount = Integer.parseInt(items[1]);
-        String startPoint = items[2];
+        int startPoint = Integer.parseInt(items[2]);
 
         graph = new HashMap<>();
 
         // 그래프 형태 만들기
         for (int i=0 ; i<lineCount ; i++) {
             items = br.readLine().split(" ");
-            String p1 = items[0];
-            String p2 = items[1];
+            int p1 = Integer.parseInt(items[0]);
+            int p2 = Integer.parseInt(items[1]);
             if (!graph.containsKey(p1))
-                graph.put(p1, new ArrayList<String>());
+                graph.put(p1, new ArrayList<>());
             if (!graph.containsKey(p2))
-                graph.put(p2, new ArrayList<String>());
+                graph.put(p2, new ArrayList<>());
             graph.get(p1).add(p2);
             graph.get(p2).add(p1);
         }
 
-        for (String s : graph.keySet())
+        for (int s : graph.keySet())
             Collections.sort(graph.get(s));
 
-        List<String> dfsList = new ArrayList<>();
+        List<Integer> dfsList = new ArrayList<>();
         dfsList.add(startPoint);
-        List<String> bfsList = new ArrayList<>();
+        List<Integer> bfsList = new ArrayList<>();
         bfsList.add(startPoint);
 
-        List<String> dfs = dfsFunc(pointCount, dfsList);
-        List<String> bfs = bfsFunc(pointCount, bfsList, 0);
+        List<Integer> bfs = bfsFunc(bfsList, 0);
+        List<Integer> dfs = dfsFunc(dfsList);
         print(dfs); print(bfs);
     }
 
-    public static List<String> dfsFunc(int max, List<String> answer){
-        String now = answer.get(answer.size()-1);
+    public static List<Integer> dfsFunc(List<Integer> answer){
+        int now = answer.get(answer.size()-1);
         if (!graph.containsKey(now))
             return answer;
-        for (String s : graph.get(now)) {
+        for (int s : graph.get(now)) {
             if (!answer.contains(s)) {
                 answer.add(s);
-                answer = dfsFunc(max, answer);
+                answer = dfsFunc(answer);
             }
         }
         return answer;
     }
 
-    public static List<String> bfsFunc(int max, List<String> answer, int idx){
-        String now = answer.get(idx);
+    public static List<Integer> bfsFunc(List<Integer> answer, int idx){
+        int now = answer.get(idx);
         if (!graph.containsKey(now))
             return answer;
-        for (String s : graph.get(now)) {
+        for (int s : graph.get(now)) {
             if (!answer.contains(s))
                 answer.add(s);
         }
 
         if (idx < answer.size()-1) {
             idx++;
-            answer = bfsFunc(max, answer, idx);
+            answer = bfsFunc(answer, idx);
         }
 
         return answer;
     }
 
-    public static void print(List<String> item){
+    public static void print(List<Integer> item){
         String value = "";
-        for (String s : item)
+        for (int s : item)
             value = value + s + " ";
 
         value = value.substring(0, value.length()-1);
