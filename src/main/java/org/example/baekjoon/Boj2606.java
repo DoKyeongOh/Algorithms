8c35cd72381ec963b019c199be0e3683ac20976d
dokyeong <dkproh@gmail.com>
2022-10-07 23:56:18 +0900

    백준2606 - 바이러스 문제 dfs로 해결 중 실행은 되었는데 답이 1차이나서 디버깅 중

--- src/main/java/org/example/baekjoon/Boj2606.java
+++ src/main/java/org/example/baekjoon/Boj2606.java
@@ -4,8 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Boj2606 {
+
+    static Map<String, List<String>> graph;
 
     public static void main(String[] arg) throws Exception {
         File file = new File("src/main/resources/input");
@@ -16,7 +22,35 @@ public class Boj2606 {
     }
 
     public static void solution(BufferedReader br) throws IOException {
-        
+        int pc = Integer.parseInt(br.readLine());
+        int lc = Integer.parseInt(br.readLine());
+        graph = new HashMap<>();
+
+        for (int i=0 ; i<lc ; i++) {
+            String[] li = br.readLine().split(" ");
+            if (!graph.containsKey(li[0]))
+                graph.put(li[0], new ArrayList<>());
+            if (!graph.containsKey(li[1]))
+                graph.put(li[1], new ArrayList<>());
+
+            graph.get(li[0]).add(li[1]);
+            graph.get(li[1]).add(li[0]);
+        }
+
+        int count = dfs(new HashMap<>(), "1");
+        System.out.println(count);
+
+    }
+
+    public static int dfs(Map<String, Boolean> map, String point){
+        if (!map.containsKey(point)) {
+            map.put(point, true);
+        }
+        for (String p : graph.get(point)) {
+            if (!map.containsKey(p))
+                dfs(map, p);
+        }
+        return map.keySet().size();
     }
 
 }
