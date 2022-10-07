ebbe57a7fc47c4f2214423fb222f39716d4e7a55
dokyeong <dkproh@gmail.com>
2022-10-07 23:33:42 +0900

    백준 - 그래프 탐색 문제 - 바이러스 추가

---
+++ src/main/java/org/example/baekjoon/Boj2606.java
@@ -0,0 +1,22 @@
+package org.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Boj2606 {
+
+    public static void main(String[] arg) throws Exception {
+        File file = new File("src/main/resources/input");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        solution(br);
+        return;
+    }
+
+    public static void solution(BufferedReader br) throws IOException {
+        
+    }
+
+}
