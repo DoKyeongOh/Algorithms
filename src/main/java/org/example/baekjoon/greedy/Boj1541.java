a613f5ff178ca11e9181d73738d4c377c6c0f6d4
dokyeong <dkproh@gmail.com>
2022-10-27 23:46:46 +0900

    백준 1541 잃어버린 괄호 클래스 생성

---
+++ src/main/java/org/example/baekjoon/greedy/Boj1541.java
@@ -0,0 +1,21 @@
+package org.example.baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Boj1541 {
+    public static void main(String[] arg) throws Exception {
+        File file = new File("src/main/resources/input");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        solution(br);
+        return;
+    }
+
+    public static void solution(BufferedReader br) throws IOException {
+        
+    }
+}
