17fb45283873f61b6584b2db22f932071be2aa48
dokyeong <dkproh@gmail.com>
2022-11-06 21:07:04 +0900

    백준 1715 카드 정렬하기 해결 힙을 사용해야 한다는 생각을 하긴 했지만 정석대로 힙을 구현하진 않았었음 -> 첫 성공 이후에 우선순위 큐를 사용하면 편하다는 것을 알아냄(자바 유틸리티 라이브러리에 있음)

--- src/main/java/org/example/baekjoon/greedy/Boj1715.java
+++ src/main/java/org/example/baekjoon/greedy/Boj1715.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Boj1715 {
 
@@ -15,49 +12,68 @@ public class Boj1715 {
         File file = new File("src/main/resources/input");
         FileReader fileReader = new FileReader(file);
         BufferedReader br = new BufferedReader(fileReader);
-        solution(br);
+        solution2(br);
         return;
     }
 
     public static void solution(BufferedReader br) throws IOException {
         int count = Integer.parseInt(br.readLine());
-        List<Integer> numList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        int answer = 0;
         int buf = 0;
-        int cValue = 0;
 
         for (int i=0 ; i<count ; i++)
-            numList.add(Integer.parseInt(br.readLine()));
+            list.add(Integer.parseInt(br.readLine()));
 
-        if (numList.size() < 2) {
+        if (list.size() < 2) {
             System.out.println(0);
             return;
         }
 
-        int size = numList.size();
-        Collections.sort(numList);
+        int size = list.size();
+        Collections.sort(list);
         for (int i=0 ; i<size ; i++) {
-            buf = numList.remove(0) + numList.remove(0);
-            cValue += buf;
-            if (putItem(numList, buf).size() == 0)
+            buf = list.remove(0) + list.remove(0);
+            answer += buf;
+
+            if (list.size() == 0)
                 break;
+
+            for (int n=list.size()-1 ; n>=0 ; n--) {
+                if (buf < list.get(n))
+                    continue;
+
+                if (n==list.size()-1)
+                    list.add(buf);
+                else
+                    list.add(n+1, buf);
+                buf = 0;
+                break;
+            }
+            if (buf != 0)
+                list.add(0, buf);
         }
-        System.out.println(cValue);
+        System.out.println(answer);
     }
 
-    public static List<Integer> putItem(List<Integer> list, int value){
-        if (value > list.get(list.size()-1)) {
-            list.add(value);
-            return list;
-        }
+    public static void solution2(BufferedReader br) throws IOException {
+        int count = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for (int i=0 ; i<list.size() ; i++) {
-            if (list.get(i) <= value)
-                continue;
+        for (int i=0 ; i<count ; i++)
+            pq.add(Integer.parseInt(br.readLine()));
 
-            list.add(i, value);
-            break;
+        int answer = 0;
+        int buf = 0;
+        for (int i=0 ; i<count ; i++) {
+            if (pq.size() < 2)
+                break;
+            buf = pq.poll() + pq.poll();
+            answer+= buf;
+            pq.add(buf);
         }
-        return list;
+        System.out.println(answer);
+
     }
 
 }
