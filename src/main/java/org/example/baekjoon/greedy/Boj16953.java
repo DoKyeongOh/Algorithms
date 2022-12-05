d2a2d3e6a49476291b6b0900869cd520f90ecb5a
DoKyeong Oh <dkproh@gmail.com>
2022-12-05 23:06:39 +0900

    백준 16953 문제 풀기위한 틀 작성

--- src/main/java/org/example/baekjoon/greedy/Boj16953.java
+++ src/main/java/org/example/baekjoon/greedy/Boj16953.java
@@ -1,4 +1,19 @@
 package org.example.baekjoon.greedy;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Boj16953 {
+    public static void main(String[] arg) throws Exception {
+        File file = new File("src/main/resources/input");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        solution(br);
+        return;
+    }
+
+    public static void solution(BufferedReader br) throws IOException {
+    }
 }
